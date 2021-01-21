a031006bba61522ba81b1fb7b1f36e386374f0c4
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2021-01-20 22:53:44 -0500

    streams

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const addon = require('addon');
 
-console.log(addon.hello())
+const {PassThrough, pipeline, Transform} = require('stream')
+const fs = require('fs')
+
+const passThrough = new PassThrough();
+const input = fs.createReadStream('pipeline_in.txt');
+const output = fs.createWriteStream('pipeline_out.txt')
+
+
+// pipeline(input, passThrough, output, err =>{
+//   if(err){
+//     console.log('pipeline has failed', err.message)
+//   }else{
+//     console.log('pipeline ended successfully')
+//   }
+// })
+const upperCaseTransform = new Transform({
+  transform(chunk, encoding, callback){
+    // chunk is converted to strinng, because it is orignaly buffer
+    callback(null, chunk.toString().toUpperCase())
+  }
+})
+
+// Takes user input and converts it to uppercase.
+pipeline(process.stdin, upperCaseTransform, process.stdout, error =>{
+  if(error) console.log('error executing pipeline', error.message)
+  else console.log('pipeline successfully executed')
+})
